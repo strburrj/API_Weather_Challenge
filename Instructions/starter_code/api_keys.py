76c15819229dcace4672dc737f9f6ca08255f088
strburrj <57110745+strburrj@users.noreply.github.com>
2020-01-14 15:58:20 -0600

    Homework

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ae273276f904707a991f7a3e21c73485"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCr_9kXcp95M7zJ2E0BbaC1P40kYyJfrsc"
